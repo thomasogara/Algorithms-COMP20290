e98d4b5944e94f88022b989e37a7e54b753010c7
thomasogara <thomas.ogara@ucdconnect.ie>
2020-03-25 19:19:27 +0000

    practical 7 code update

---
+++ Practical-7/Practical_7/Main.java
@@ -0,0 +1,10 @@
+package Practical_7;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] testStrings = readTestFiles();
+        Stiing[] testPatterns = readPatternFiles();
+    }
+}
